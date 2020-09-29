d05755a0324a53309b85a972a228a591b410a004
Jackson Xiao <jacksonxiao0@gmail.com>
2020-09-29 00:07:55 -0400

    Updates

---
+++ precache-manifest.b68917ae73af7cfa9361664c7b387828.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b019b038c81875d7368a3d7ccc1600c",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "67d6c576c448481e20b5",
+    "url": "/tic-tac-toe/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "fe82095b8ddfffb8a898",
+    "url": "/tic-tac-toe/static/css/main.12989bf5.chunk.css"
+  },
+  {
+    "revision": "67d6c576c448481e20b5",
+    "url": "/tic-tac-toe/static/js/2.a4d48e79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.a4d48e79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe82095b8ddfffb8a898",
+    "url": "/tic-tac-toe/static/js/main.ffd80a40.chunk.js"
+  },
+  {
+    "revision": "61109fc60463736a7ada",
+    "url": "/tic-tac-toe/static/js/runtime-main.12138588.js"
+  }
+]);
